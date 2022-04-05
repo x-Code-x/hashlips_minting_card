998f15db8f621a54b39258da79e1dbafcbfec126
Code X <724723+x-Code-x@users.noreply.github.com>
2022-04-05 18:00:03 +1000

    Update Minter.js

--- src/components/Minter.js
+++ src/components/Minter.js
@@ -3,7 +3,6 @@ import Web3 from "web3";
 import contract from "../contracts/contract.json";
 import Hero from "../assets/hero.png";
 import Logo from "../assets/logo.png";
-import Backgound from ../assets/spacetoon-bg.jpg";
 
 const initialInfoState = {
   connected: false,

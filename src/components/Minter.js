330780961291c4128cec36cdd0ed8c58d116355b
Code X <724723+x-Code-x@users.noreply.github.com>
2022-04-07 12:26:47 +1000

    Update Minter.js

--- src/components/Minter.js
+++ src/components/Minter.js
@@ -280,9 +280,7 @@ function Minter() {
               <div style={{ display: "flex", justifyContent: "space-between" }}>
                 <p style={{ color: "var(--statusText)", textAlign: "center" }}>
                   {Number(
-                    const val = 0.25
-                    var weiAmount = ethers.utils.parseEther("0.25");
-                      info.web3?.utils.fromWei(weiAmount, "ether") * mintInfo.amount
+                      info.web3?.utils.fromWei(ethers.utils.parseEther("0.25"), "ether") * mintInfo.amount
                   ).toFixed(3)}{" "}
                   {contract.chain_symbol}
                 </p>

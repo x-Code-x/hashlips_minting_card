bfa446582a4fd88511f5d6fecb794862ba1d01aa
Code X <724723+x-Code-x@users.noreply.github.com>
2022-04-07 19:14:33 +1000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Minter from "./components/Minter";
+import Reveal from "./components/Reveal";
 import "./styles/styles.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <Minter />
+    <Reveal />
   </React.StrictMode>,
   document.getElementById("root")
 );

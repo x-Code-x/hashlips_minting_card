67c2f7389a0fb6e66bcad045de4aee4953bc4714
Code X <724723+x-Code-x@users.noreply.github.com>
2022-04-08 18:29:02 +1000

    Update Reveal.js

--- src/components/Reveal.js
+++ src/components/Reveal.js
@@ -1,1 +1,12 @@
+import React, { Component } from "react";
+import ReactDOM, { createPortal } from "react-dom";
 
+export default class Portal extends Component {
+  render() {
+    const { selector } = this.props;
+    return ReactDOM.createPortal(
+      this.props.children,
+      document.querySelector(selector)
+    );
+  }
+}

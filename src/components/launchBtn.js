6488e639e021d689bb52968cdeab35a2b48fb64e
Code X <724723+x-Code-x@users.noreply.github.com>
2022-04-08 23:10:54 +1000

    Create launchBtn.js

---
+++ src/components/launchBtn.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { createPortal } from "react-dom";
+import SnackBar from "./snackbar";
+
+const LaunchBtn = props => {
+  const [showSnack, setShowSnack] = useState(false);
+  const [fixedSnack, setFixedSnack] = useState(false);
+
+  const dismissFn = () => setShowSnack(false);
+  const toggleFixedSnack = () => setFixedSnack(!fixedSnack);
+
+  return (
+    <div className="launcher__container">
+      <span className="launcher__message">Click to trigger a message!</span>
+      <button
+        className="launcher__button"
+        onClick={() => {
+          setShowSnack(true);
+          if (!fixedSnack) {
+            setTimeout(() => {
+              setShowSnack(false);
+            }, 2500);
+          }
+        }}
+      >
+        Click me!
+      </button>
+      <div className="launcher__fixed-selector">
+        <input id="fixedsnackbar" type="checkbox" onChange={toggleFixedSnack} />
+        <label htmlFor="fixedsnackbar">Fixed</label>
+      </div>
+      {showSnack &&
+        createPortal(
+          <SnackBar dismiss={dismissFn} />,
+          document.getElementById("supersnackbarcontainer")
+        )}
+    </div>
+  );
+};
+
+export default LaunchBtn;

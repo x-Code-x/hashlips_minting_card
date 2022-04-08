b5bbf0ac18873deecd97baec3887b7f110b621b5
Code X <724723+x-Code-x@users.noreply.github.com>
2022-04-08 19:58:55 +1000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "antd/dist/antd.css";
+import { Button, Card } from "antd";
+
 import Minter from "./components/Minter";
 import Reveal from "./components/Reveal";
 import "./styles/styles.css";

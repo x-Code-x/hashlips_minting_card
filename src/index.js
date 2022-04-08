5f5b84bc210c58284d9ff4f997f5dd574b7a1764
Code X <724723+x-Code-x@users.noreply.github.com>
2022-04-08 18:41:08 +1000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Minter from "./components/Minter";
+import Reveal from "./components/Reveal";
 import "./styles/styles.css";
 
 ReactDOM.render(

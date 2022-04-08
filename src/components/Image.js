2fe24788548c6f91df85efe8bf1fcd57d26fab3c
Code X <724723+x-Code-x@users.noreply.github.com>
2022-04-08 22:00:38 +1000

    Update Image.js

--- src/components/Image.js
+++ src/components/Image.js
@@ -4,7 +4,7 @@ const Image = () => (
   <img
     alt="should be cropped by container overflow:hidden style"
     style={styles.image}
-    src="https://cdn.pixabay.com/photo/2019/11/29/14/18/gent-4661419_960_720.jpg"
+    src="https://gateway.pinata.cloud/ipfs/QmeZksEGADrQTerGB83oFUqtfYVAMJ71JS43a5fNLPq1ax/000000000000000000000000000000000000000000000000000000000bronze2.png"
   />
 );
 

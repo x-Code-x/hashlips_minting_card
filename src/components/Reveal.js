6ea91d9440ee16e3045083ef22317c30219048ae
Code X <724723+x-Code-x@users.noreply.github.com>
2022-04-09 02:42:52 +1000

    Update Reveal.js

--- src/components/Reveal.js
+++ src/components/Reveal.js
@@ -1,21 +1,13 @@
 import React from "react";
 import "../styles/styles.css";
 
-//export default function Reveal() {
-//  return (
-//    <div className="Reveal">
-//      <h1>Reveal</h1>
-//      <div className="coin"></div>
-//    </div>
-//  );
-//}
-const TicketBar = ({ message = "Hello world!", dismiss }) => (
-  <div className="ticketbar__container">
-    <span className="ticketbar__message">{message}</span>
-    <span className="ticketbar__dismiss" icon="fa-close" onClick={dismiss}>
-      x
-    </span>
-  </div>
-);
+export default function Reveal() {
+  return (
+    <div className="Reveal">
+      <h1>Reveal</h1>
+      <div className="coin"></div>
+    </div>
+  );
+}
 
-export default TicketBar;
+export default Reveal;

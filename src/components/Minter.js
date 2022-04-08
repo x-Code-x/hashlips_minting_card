774b9e3d2658f94e1fd8acc06363817c01809268
Code X <724723+x-Code-x@users.noreply.github.com>
2022-04-08 23:21:55 +1000

    Update Minter.js

--- src/components/Minter.js
+++ src/components/Minter.js
@@ -4,7 +4,7 @@ import { ethers } from "ethers";
 import contract from "../contracts/contract.json";
 import Hero from "../assets/hero.png";
 import Logo from "../assets/logo.png";
-import LaunchBtn from "./components/launchBtn";
+import LaunchBtn from "./launchBtn";
 
 const initialInfoState = {
   connected: false,

4640f9d5760c8d13944cc37fcb9ba65182aa0cad
Code X <724723+x-Code-x@users.noreply.github.com>
2022-04-08 23:10:09 +1000

    Update Reveal.js

--- src/components/Reveal.js
+++ src/components/Reveal.js
@@ -1,10 +1,21 @@
+import React from "react";
 import "../styles/styles.css";
 
-export default function Reveal() {
-  return (
-    <div className="Reveal">
-      <h1>Reveal</h1>
-      <div className="coin"></div>
-    </div>
-  );
-}
+//export default function Reveal() {
+//  return (
+//    <div className="Reveal">
+//      <h1>Reveal</h1>
+//      <div className="coin"></div>
+//    </div>
+//  );
+//}
+const SnackBar = ({ message = "Hello world!", dismiss }) => (
+  <div className="snackbar__container">
+    <span className="snackbar__message">{message}</span>
+    <span className="snackbar__dismiss" icon="fa-close" onClick={dismiss}>
+      x
+    </span>
+  </div>
+);
+
+export default SnackBar;

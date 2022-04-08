28d2f87f118a056a9d2c43ae39242a332fa51296
Code X <724723+x-Code-x@users.noreply.github.com>
2022-04-08 22:56:01 +1000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import "./styles/styles.css";
 import Minter from "./components/Minter";
 import TooltipPopover from "./components/TooltipPopover";
 import Reveal from "./components/Reveal";
-import Image from "./components/Image";
 
 ReactDOM.render(
   <React.StrictMode>
